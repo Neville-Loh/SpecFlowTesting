fa8bfec9d2e7ed2cd0f4f6caa8785a7f8d83824c
Neville <nevilleloh0115@gmail.com>
2020-10-25 12:07:55 +1300

    initial commit

---
+++ SpecflowNetCoreDemo/Feature/Login.feature
@@ -0,0 +1,12 @@
+﻿Feature: Login
+	Login to EA Demo application
+
+@mytag
+Scenario: Perform Login of EA Application site
+	Given I launch the application
+	And I click login link
+	When I enter the following details
+		| UserName | Password |
+		| admin    | password |
+	And I click login button
+	Then I should see Employee details link
---
+++ SpecflowNetCoreDemo/Steps/LoginSteps.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using SpecflowNetCoreDemo.Pages;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+
+namespace SpecflowNetCoreDemo.Steps
+{
+    [Binding]
+    public sealed class LoginSteps
+    {
+        //Anti-Context Injection code - 100% wrong??
+        LoginPage loginPage = null;
+
+
+        [Given(@"I launch the application")]
+        public void GivenILaunchTheApplication()
+        {
+            IWebDriver webDriver = new ChromeDriver();
+            webDriver.Navigate().GoToUrl("http://eaapp.somee.com/");
+            loginPage = new LoginPage(webDriver);
+        }
+
+        [Given(@"I click login link")]
+        public void GivenIClickLoginLink()
+        {
+            loginPage.ClickLogin();
+        }
+
+        [When(@"I enter the following details")]
+        public void WhenIEnterTheFollowingDetails(Table table)
+        {
+            dynamic data = table.CreateDynamicInstance();
+            loginPage.Login((string) data.UserName, (string) data.Password);
+        }
+
+        [When(@"I click login button")]
+        public void WhenIClickLoginButton()
+        {
+            loginPage.ClickLoginButton();
+        }
+
+        [Then(@"I should see Employee details link")]
+        public void ThenIShouldSeeEmployeeDetailsLink()
+        {
+            Assert.That(loginPage.IsEmployeeDetailsExist(), Is.True);
+        }
+
+    }
+}
